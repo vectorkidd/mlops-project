3cec8aa9578d8ab17d8149389d82be5cb7d34bc2
Khalid <khalid.m8746@gmail.com>
2026-02-04 20:45:50 +0300

    Data ingestion completed

--- demo.py
+++ demo.py
@@ -1,9 +1,14 @@
-from src.logger import logging
-from src.exception import MyException
-import sys
+# from src.logger import logging
+# from src.exception import MyException
+# import sys
 
-try:
-    a=1+'2'
-except Exception as e:
-    logging.info(f"We are logging the error now{e}")
-    raise MyException(e, sys) from e
+# try:
+#     a=1+'2'
+# except Exception as e:
+#     logging.info(f"We are logging the error now{e}")
+#     raise MyException(e, sys) from e
+
+from src.pipline.training_pipeline import TrainPipeline
+
+pipeline = TrainPipeline()
+pipeline.run_pipeline()
